aa410e2819a2f4f18c34b733cb971374e6679ceb
pranav <pranavmoon@gmail.com>
2018-08-14 13:57:15 +0530

    plot3

---
+++ plot3.R
@@ -0,0 +1,32 @@
+## Course: EDA-week-1Project
+rm("exdata_data_household_power_consumption.zip")
+rm("household_power_consumption")
+dataurl<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+filename<-"exdata_data_household_power_consumption.zip"
+download.file(dataurl,filename)
+unzip("exdata_data_household_power_consumption.zip")
+##-----------------------------------------------------
+library(data.table)
+c4w1p1<-fread("household_power_consumption.txt")
+str(c4w1p1)
+print("We will only be using data from the dates 2007-02-01 and 2007-02-02.")
+c4w1p1_sub<-subset(c4w1p1, Date=="1/2/2007"|Date=="2/2/2007")
+
+c4w1p1_sub<-transform(c4w1p1_sub,Sub_meterning_1=as.numeric(Sub_metering_1))
+c4w1p1_sub<-transform(c4w1p1_sub,Sub_meterning_2=as.numeric(Sub_metering_2))
+c4w1p1_sub<-transform(c4w1p1_sub,Sub_meterning_3=as.numeric(Sub_metering_3))
+
+str(c4w1p1_sub)
+par(mfrow=c(1,1))
+library(gridExtra)
+png("plot3.png",width=480,height=480)
+
+plot(c4w1p1_sub$Sub_metering_1,type="l",ylab="Energy sub metering",xlab=" ",xaxt="n")
+#tapply(c4w1p1_sub$Sub_metering_1, c4w1p1_sub$Day=="Thu"|c4w1p1_sub$Day=="Fri",plot,type="l",ylab="Energy sub metering",xlab=" ",xaxt="n")
+lines(c4w1p1_sub$Sub_metering_2,col="red")
+lines(c4w1p1_sub$Sub_metering_3,col="blue")
+axis(1,at=c(0,1441,2880),labels = c("Thu","Fri","Sat"))
+legend("topright",lty=1,xjust=1,cex=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+#dev.copy(png, width=480,height=480,file="plot3_ex.png")
+dev.off()
