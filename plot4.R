f5ed2e0076cdf768c38fa5dd180857c64db3d745
pranav <pranavmoon@gmail.com>
2018-08-20 13:07:37 +0530

    plot4.R_v2

--- plot4.R
+++ plot4.R
@@ -19,23 +19,25 @@ c4w1p1_sub<-transform(c4w1p1_sub,Sub_meterning_3=as.numeric(Sub_metering_3))
 c4w1p1_sub<-transform(c4w1p1_sub,Voltage=as.numeric( Voltage))
 c4w1p1_sub<-transform(c4w1p1_sub,Global_reactive_power=as.numeric(Global_reactive_power))
 
+library(dplyr)
+
+#add a column of date and time togegher
+c4w1p1_sub[,datetime:=paste(c4w1p1_sub$Date,c4w1p1_sub$Time,sep=" ")]  
+c4w1p1_sub<-transform(c4w1p1_sub,datetime=as.POSIXct(datetime, format="%d/%m/%Y %H:%M:%S"))
+
 library(gridExtra)
 
 png("plot4.png",width=480,height=480)
 
 par(mfrow=c(2,2))
 with(c4w1p1_sub, 
-     {plot(Global_active_power,type="l",ylab="Global Active Power",xlab=" ",xaxt="n")
-       axis(1,at=c(0,1441,2880),labels = c("Thu","Fri","Sat"))
-       plot(Voltage,type="l",ylab="Voltage",xlab="datetime ",xaxt="n")
-       axis(1,at=c(0,1441,2880),labels = c("Thu","Fri","Sat"))
-       plot(Sub_metering_1,type="l",ylab="Energy sub metering",xlab=" ",xaxt="n")
-       lines(c4w1p1_sub$Sub_metering_2,col="red")
-       lines(c4w1p1_sub$Sub_metering_3,col="blue")
+     {plot(Global_active_power~c4w1p1_sub$datetime,type="l",ylab="Global Active Power",xlab=" ")
+       plot(Voltage~c4w1p1_sub$datetime,type="l",ylab="Voltage",xlab="datetime ")
+       plot(Sub_metering_1~c4w1p1_sub$datetime,type="l",ylab="Energy sub metering",xlab=" ")
+       lines(c4w1p1_sub$Sub_metering_2~c4w1p1_sub$datetime,col="red")
+       lines(c4w1p1_sub$Sub_metering_3~c4w1p1_sub$datetime,col="blue")
        legend("topright",lty=1,cex=.9,bty="n",col=c("black","red","blue"),legend=c("Sub_meterning_1","Sub_meterning_2","Sub_meterning_3"))
-       axis(1,at=c(0,1441,2880),labels = c("Thu","Fri","Sat"))
-       plot(Global_reactive_power,type="l",ylab="Global_reactive_power",xlab="datetime ",xaxt="n")
-       axis(1,at=c(0,1441,2880),labels = c("Thu","Fri","Sat"))
+       plot(Global_reactive_power~c4w1p1_sub$datetime,type="l",ylab="Global_reactive_power",xlab="datetime ")
      })  
 #dev.copy(png, width=480,height=480,file="plot4_ex.png")
 dev.off()
