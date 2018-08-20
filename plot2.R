602440ba2b636ca90c0595a491fdec2ddfbc7f54
pranav <pranavmoon@gmail.com>
2018-08-20 12:07:20 +0530

    plot2.R_v2

--- plot2.R
+++ plot2.R
@@ -11,17 +11,17 @@ c4w1p1<-fread("household_power_consumption.txt")
 
 print("We will only be using data from the dates 2007-02-01 and 2007-02-02.")
 
-# c4w1p1_sub<-c4w1p1[which(c4w1p1[,"Date"]=="1/2/2007"|c4w1p1[,"Date"]=="2/2/2007"),]
-c4w1p1_sub<-subset(c4w1p1, Date=="1/2/2007"|Date=="2/2/2007")
+c4w1p1_sub<-subset(c4w1p1, Date=="1/2/2007"|Date=="2/2/2007")  #data: 1/2/07 t0 2/2/07
 
 c4w1p1_sub<-transform(c4w1p1_sub,Global_active_power=as.numeric(Global_active_power))
 
 library(dplyr)
 
-#c4w1p1_sub[,Day:=wday(dmy(c4w1p1_sub$Date),label=T)]  #data: 1/2/07 t0 2/2/07
-#tapply(c4w1p1_sub$Global_active_power, c4w1p1_sub$Day=="Thu"|c4w1p1_sub$Day=="Fri",plot,type="l",ylab="Global Active Power (kilowatts)",xlab=" ",xaxt="n")
+#add a column of date and time togegher
+c4w1p1_sub[,datetime:=paste(c4w1p1_sub$Date,c4w1p1_sub$Time,sep=" ")]  
+c4w1p1_sub<-transform(c4w1p1_sub,datetime=as.POSIXct(datetime, format="%d/%m/%Y %H:%M:%S"))
 par(mfrow=c(1,1))
-plot(c4w1p1_sub$Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=" ",xaxt="n")
-axis(1,at=c(0,1441,2880),labels = c("Thu","Fri","Sat"))
+plot(c4w1p1_sub$Global_active_power~c4w1p1_sub$datetime ,type="l",ylab="Global Active Power (kilowatts)",xlab=" ")
+
 dev.copy(png, file="plot2.png")
 dev.off()
